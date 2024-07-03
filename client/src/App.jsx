bf18742aef092ca739609fcc54fa4135200fcae6
Dickson Mayoyo <onseriodickson2@gmail.com>
2024-07-03 13:33:03 +0300

    Added pages to the project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Signin from "./pages/Signin"
+import Signup from "./pages/Signup"
+import Profile from "./pages/Profile"
 
 function App() {
   return (
-    <div className='text-green-700'>App</div>
+<BrowserRouter>
+<Routes>
+  <Route path="/" element={<Home/>}/>
+  <Route path="/about" element={<About/>}/>
+  <Route path="/signin" element={<Signin/>}/>
+  <Route path="/signup" element={<Signup/>}/>
+  <Route path="/profile" element={<Profile/>}/>
+</Routes>
+</BrowserRouter>
   )
 }
 
