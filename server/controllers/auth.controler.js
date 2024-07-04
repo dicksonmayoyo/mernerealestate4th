250f2dbb0d7c4c72141550a93d759442d8e2c7a1
Dickson Mayoyo <onseriodickson2@gmail.com>
2024-07-04 15:39:28 +0300

    Completed Sign up ui

--- server/controllers/auth.controler.js
+++ server/controllers/auth.controler.js
@@ -1,6 +1,6 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs'
-import { errorhandler } from "../utils/error.js";
+
 export const signup= async (req, res, next)=>{
   const {username, email, password}=req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10)
