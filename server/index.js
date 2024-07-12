71a0b21ea156f0a01c415976ba8e27b17d87127a
Dickson Mayoyo <onseriodickson2@gmail.com>
2024-07-12 10:57:16 +0300

    Create user update api

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import authRouter from './routes/auth.route.js'
 import userRouter from './routes/user.route.js'
+import cookieParser from 'cookie-parser' 
 dotenv.config();
 
 
@@ -13,6 +14,7 @@ console.log(err);
 })
 const app = express()
 app.use(express.json())
+app.use(cookieParser())
 app.listen(3000, ()=>{
    console.log("Server is running on port 3000!!"); 
 })
---
+++ server/utils/verifyUser.js
@@ -0,0 +1,12 @@
+import { errorhandler } from "./error.js";
+import jwt from 'jsonwebtoken'
+
+export const verifyToken = (req, res, next)=>{
+    const token = req.cookies.access_token;
+    if(!token) return next(errorhandler(401, 'Unauthorized'))
+     jwt.verify(token, process.env.JWT_SECRET, (err, user)=>{
+    if(err) return next(errorhandler(403, 'Forbiden'))
+        req.user = user;
+    next();
+    })
+}
